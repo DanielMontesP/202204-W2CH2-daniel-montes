f96953b14d783584d539b32b245f4bbe1cb15d5c
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-12 20:44:19 +0200

    Added results for each case

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-//aqui va el cod js
+// console.log(strictEquals(1, 1));  TRUE!
+//console.log(strictEquals(NaN, NaN)); FALSE?
+// console.log(strictEquals(0, -0)); TRUE?
+//console.log(strictEquals(-0, 0)); TRUE?
+//console.log(strictEquals(1, "1")); FALSE!
+//console.log(strictEquals(true, false)); FALSE!
+//console.log(strictEquals(true, false)); FALSE!
+//console.log(strictEquals(false, false)); TRUE!
+//console.log(strictEquals("Water", "oil")); FALSE!
+//PRUEBAS
+
 function strictEquals(a, b) {
   if (Object.is(a, b)) {
     return true;
   }
+  return false;
 }
 
-console.log(strictEquals(1, 1));
+console.log(strictEquals(-0, 0));
