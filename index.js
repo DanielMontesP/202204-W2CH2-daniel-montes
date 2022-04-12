f3634a2463f346cd330c2e214f9f6fede9c5b70b
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-12 21:38:01 +0200

    Added jest function to index.js

--- index.js
+++ index.js
@@ -9,11 +9,38 @@
 //console.log(strictEquals("Water", "oil")); FALSE!
 //PRUEBAS
 
+const { describe } = require("yargs");
+
 function strictEquals(a, b) {
+  describe(
+    "When compared: " + a + " with: " + b + " strictEquals function returns",
+    () => {
+      test("Test expected results", () => {
+        switch (a) {
+          case 1:
+            switch (b) {
+              case 1:
+                expect(Object.is(a, b)).toBe(true);
+                break;
+              case "1":
+                expect(Object.is(a, b)).toBe(false);
+                break;
+            }
+            break;
+          case Nan:
+            if (b === Nan) {
+              expect(Object.is(a, b)).toBe(false);
+            }
+            break;
+        }
+      });
+    }
+  );
   if (Object.is(a, b)) {
     return true;
   }
   return false;
+  describe;
 }
 
 console.log(strictEquals(-0, 0));
